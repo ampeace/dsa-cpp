d730aee7d0ef1db759cc8cdd6dcb85e79b62062f
ampeace <aman.kumar.singh.541@gmail.com>
2026-03-31 12:23:48 +0530

    2D Array implementation in cpp

---
+++ Arrays/2DArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[2][2];
+
+    // Input
+    cout << "Enter 4 elements:\n";
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Output
+    cout << "Matrix is:\n";
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
