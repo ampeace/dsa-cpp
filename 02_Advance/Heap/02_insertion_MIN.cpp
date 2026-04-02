b398d44c3306c119b12afe994a4f8687bc36fed4
ampeace <aman.kumar.singh.541@gmail.com>
2026-04-02 13:49:51 +0530

    added files in heap

--- 02_Advance/Heap/02_insertion_MIN.cpp
+++ 02_Advance/Heap/02_insertion_MIN.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main(){
-    priority_queue<int, vector<int>
+    priority_queue<int, vector<int>, greater<int>>pq; //min-heap
+    int n , x;
+    cout<<"Enter number of elements: ";
+    cin>>n;
+    cout<<"Enter elements:\n";
+    for(int i =0; i<n;i++){
+        cin>>x;
+        pq.push(x);
+    }
+    cout<<"Heap elements (smallest to largest):\n";
+    while(!pq.empty()){
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+    return 0;
 }
