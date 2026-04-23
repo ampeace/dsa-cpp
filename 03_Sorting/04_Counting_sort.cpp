c5cabb325057eaccf745f4bc5e610ef8f6835e62
ampeace <aman.kumar.singh.541@gmail.com>
2026-04-23 13:54:38 +0530

    Updated

---
+++ 03_Sorting/04_Counting_sort.cpp
@@ -0,0 +1,36 @@
+// Counting Sort Implementation
+#include <iostream>
+using namespace std;
+
+void countingSort(int arr[], int n, int k) {
+    int count[k + 1];
+    for (int i = 0; i <= k; i++) {
+        count[i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+    for (int i = 1; i <= k; i++) {
+        count[i] += count[i - 1];
+    }
+    int output[n];
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+int main() {
+    int arr[] = {4, 2, 2, 8, 3, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 8;
+    countingSort(arr, n, k);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
